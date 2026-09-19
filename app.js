29f7daa43511235fa2436f72f8a4d44f0b01f5f2
chiqq <nueh2549@gmail.com>
2026-09-20 06:26:32 +0700

    Implement weather search functionality and improve loading display

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const apiKey = CONFIG.API_KEY;
 const baseUrl = 'https://api.openweathermap.org/data/2.5/weather'
-const cityInput = document.getElementById('cityInput');
 
 async function testAPI() {
 
@@ -18,16 +17,82 @@ async function testAPI() {
     console.log(data);
 }
 
-testAPI();
+// testAPI();
 
-// async function fetchWeather(cityInput) {
+const searchBtn = document.getElementById('search-btn');
+const cityInput = document.getElementById('cityInput');
 
-//     const url = 'https://api.openweathermap.org/data/4.0/onecall/current?lat={lat}&lon={lon}&appid=${API key}'
+searchBtn.addEventListener('click', function() {
 
-// }
+    const city = cityInput.value.trim();
+    if(city === "") {
+        showError();
+        return;
+    } else {
+        testAPIwithInput(city);
+        return;
+    }
+})
+
+async function testAPIwithInput(cityInput) {
+
+    // const url = baseUrl + `?q=Bangkok` + `&appid=${apiKey}` + `&units=metric` + `&lang=th`;
+    const url =
+        `https://api.openweathermap.org/data/2.5/weather` +
+        `?q=${encodeURIComponent(cityInput)}` +
+        `&appid=${apiKey}` +
+        `&units=metric` +
+        `&lang=th`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data);
+}
+
+async function fetchWeather(cityInput) {
+
+    const url =
+        `https://api.openweathermap.org/data/2.5/weather` +
+        `?q=${encodeURIComponent(cityInput)}` +
+        `&appid=${apiKey}` +
+        `&units=metric` +
+        `&lang=th`;
+
+    try {
+        const response = await fetch(url);
+        if(!response.ok) {
+            throw new Error('เกิดข้อผิดพลาด : ${response.status')
+        }
+        const data = await response.json();
+        return data
+
+    } catch (error) {
+        throw error;
+    }
+}
 
 const loading = document.getElementById('loading');
+const weatherResult = document.getElementById('weatherResult');
 const error = document.getElementById('error');
+
+function displayWeather(data, city) {
+
+    const temp = data.main.temp;
+    const humidity = data.main.humidity;
+    const wind = data.wind.speed;
+}
+
+
+function showLoading() {
+    loading.classList.remove('hidden');
+    loading.innerHTML = 'กำลังโหลดข้อมูล'
+}
+
+function showError() {
+    error.classList.remove('hidden');
+    error.innerHTML = 'กรุณากรอกชื่อเมือง'
+}
 
 // async function loadingWeather(cityInput) {
 
