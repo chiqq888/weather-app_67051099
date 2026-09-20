b72f725ece8fa3368a49a0935f52dad763a4b1a6
chiqq <nueh2549@gmail.com>
2026-09-20 07:18:32 +0700

    Refactor weather fetching functions and enhance display layout

--- app.js
+++ app.js
@@ -1,24 +1,6 @@
 const apiKey = CONFIG.API_KEY;
 const baseUrl = 'https://api.openweathermap.org/data/2.5/weather'
 
-async function testAPI() {
-
-    // const url = baseUrl + `?q=Bangkok` + `&appid=${apiKey}` + `&units=metric` + `&lang=th`;
-    const url =
-        `https://api.openweathermap.org/data/2.5/weather` +
-        `?q=Bangkok` +
-        `&appid=${apiKey}` +
-        `&units=metric` +
-        `&lang=th`;
-
-    const respone = await fetch(url);
-    const data = await respone.json();
-
-    console.log(data);
-}
-
-// testAPI();
-
 const searchBtn = document.getElementById('search-btn');
 const cityInput = document.getElementById('cityInput');
 
@@ -29,26 +11,44 @@ searchBtn.addEventListener('click', function() {
         showError();
         return;
     } else {
-        testAPIwithInput(city);
+        // testAPIwithInput(city);
+        fetchWeather(city);
+        // console.log(fetchWeather(city));
         return;
     }
 })
 
-async function testAPIwithInput(cityInput) {
+// async function testAPI() {
 
-    // const url = baseUrl + `?q=Bangkok` + `&appid=${apiKey}` + `&units=metric` + `&lang=th`;
-    const url =
-        `https://api.openweathermap.org/data/2.5/weather` +
-        `?q=${encodeURIComponent(cityInput)}` +
-        `&appid=${apiKey}` +
-        `&units=metric` +
-        `&lang=th`;
+//     const url =
+//         `https://api.openweathermap.org/data/2.5/weather` +
+//         `?q=Bangkok` +
+//         `&appid=${apiKey}` +
+//         `&units=metric` +
+//         `&lang=th`;
 
-    const response = await fetch(url);
-    const data = await response.json();
+//     const respone = await fetch(url);
+//     const data = await respone.json();
 
-    console.log(data);
-}
+//     console.log(data);
+// }
+
+// // testAPI();
+
+// async function testAPIwithInput(cityInput) {
+
+//     const url =
+//         `https://api.openweathermap.org/data/2.5/weather` +
+//         `?q=${encodeURIComponent(cityInput)}` +
+//         `&appid=${apiKey}` +
+//         `&units=metric` +
+//         `&lang=th`;
+
+//     const response = await fetch(url);
+//     const data = await response.json();
+
+//     console.log(data);
+// }
 
 async function fetchWeather(cityInput) {
 
@@ -65,7 +65,9 @@ async function fetchWeather(cityInput) {
             throw new Error('เกิดข้อผิดพลาด : ${response.status')
         }
         const data = await response.json();
-        return data
+        // console.log(data)
+        displayWeather(data);
+        // return data;
 
     } catch (error) {
         throw error;
@@ -76,11 +78,27 @@ const loading = document.getElementById('loading');
 const weatherResult = document.getElementById('weatherResult');
 const error = document.getElementById('error');
 
-function displayWeather(data, city) {
+function displayWeather(data) {
 
+    const name = data.name;
     const temp = data.main.temp;
     const humidity = data.main.humidity;
     const wind = data.wind.speed;
+    const html = `
+        <div class="weatherCard">
+            <div class="header">
+                <p><b>This is Weather Info in</b></p>
+                <p>${name}</p>
+            </div>
+            <div class="info">
+                <p><b>Temperature :</b> ${temp} °C </p>
+                <p><b>Humidity :</b> ${humidity} % </p>
+                <p><b>Wind Speed :</b> ${wind} m/s </p>
+            </div>
+        </div> 
+        `
+    weatherResult.classList.remove('hidden')
+    weatherResult.innerHTML = html;
 }
 
 
